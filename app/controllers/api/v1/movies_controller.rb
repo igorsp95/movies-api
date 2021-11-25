20e99225037783db06fb5a3e68ba8fe4f028b643
igorparente <igorscheidegger@hotmail.com>
2021-11-25 18:56:41 -0300

    redirecting

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,16 +1,16 @@
 module Api
   module V1
     class MoviesController < ApplicationController
-      # def index
-      #   if params[:query].present?
-      #     sql_query = "genre ILIKE :query OR release_year ILIKE :query OR country ILIKE :query"
-      #     @movies = Movie.where(sql_query, query: "%#{params[:query]}%")
-      #     render json: { status: 'SUCCESS', message: "Loaded movies by #{params[:query]}", data: @movies}, status: :ok
-      #   else
-      #     @movies = Movie.order('release_year DESC')
-      #     render json: { status: 'SUCCESS', message: 'Loaded movies', data: @movies}, status: :ok
-      #   end
-      # end
+      def index
+        if params[:query].present?
+          sql_query = "genre ILIKE :query OR release_year ILIKE :query OR country ILIKE :query"
+          @movies = Movie.where(sql_query, query: "%#{params[:query]}%")
+          render json: { movie_filter: "#{params[:query]}", data: @movies}, status: :ok
+        else
+          @movies = Movie.order('release_year DESC')
+          render json: { data: @movies}, status: :ok
+        end
+      end
 
       def import
         Movie.import(params[:file])
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,7 @@ class Movie < ApplicationRecord
 
   def self.import(file)
     CSV.foreach(file.path, 'r:bom|utf-8', headers: true, col_sep: ',', quote_char: '"') do |row|
-       Movie.where(title: row[1]).first_or_create! row.to_hash
+       Movie.where(title: row[1]).create! row.to_hash
     end
   end
 end
